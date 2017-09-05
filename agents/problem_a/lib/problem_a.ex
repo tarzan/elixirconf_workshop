7e50d59617a88e7ef973281d44e002fc52dee526
Maarten Jacobs <m.jacobs@defacto.nl>
2017-09-05 10:56:21 -0700

    solve agents problem_a

--- agents/problem_a/lib/problem_a.ex
+++ agents/problem_a/lib/problem_a.ex
@@ -14,6 +14,22 @@ defmodule ProblemA do
   Fetch a value from the agent.
   """
   def fetch!(agent, key) do
-    Agent.get(agent, Map, :fetch!, [key])
+    case fetch(agent, key) do
+      {:ok, value} -> value
+      {:error, e} -> raise e
+    end
+  end
+
+  defp fetch(agent, key) do
+    Agent.get(agent, fn (state) ->
+      try do
+        Map.fetch! state, key
+      rescue
+        e in [KeyError] ->
+          {:error, e}
+      else
+        val -> {:ok, val}
+      end
+    end)
   end
 end
