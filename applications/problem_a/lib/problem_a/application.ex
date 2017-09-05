92901f3a26f35df467353ec9c9389678f4e2b902
Maarten Jacobs <m.jacobs@defacto.nl>
2017-09-05 16:13:57 -0700

    solve applications problem_a

--- applications/problem_a/lib/problem_a/application.ex
+++ applications/problem_a/lib/problem_a/application.ex
@@ -6,6 +6,11 @@ defmodule ProblemA.Application do
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([Data, Alice, Bob], [strategy: :one_for_one])
+    supervisor = %{id: Supervisor,
+                   start: { Supervisor, :start_link,
+                            [[Alice, Bob], [strategy: :one_for_one]] },
+                   type: :supervisor}
+
+    Supervisor.start_link([Data, supervisor], [strategy: :one_for_all])
   end
 end
