0aaa8959e1dac42772ef0a544ab876a742ea11da
Maarten Jacobs <m.jacobs@defacto.nl>
2017-09-05 14:18:54 -0700

    solve supervisors problem_a

--- supervisors/problem_a/lib/problem_a.ex
+++ supervisors/problem_a/lib/problem_a.ex
@@ -2,11 +2,16 @@ defmodule ProblemA do
   @moduledoc """
   ProblemA.
   """
+  defmodule ChildTask do
+    use Task, restart: :transient
 
+    def start_link(fun), do: Task.start_link(fun)
+  end
   @doc """
   Start a task that is run again if it crashes
   """
   def start_link(fun) do
-    Task.start_link(fun)
+    Supervisor.start_link([{ChildTask, fun}],
+                          [strategy: :one_for_one, max_restarts: 1])
   end
 end
